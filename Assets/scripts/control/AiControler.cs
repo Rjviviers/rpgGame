d40a6f26b61dea8989d704a3dbe3243b375bde82
ruan <rjviviers@gmail.com>
2020-02-02 18:20:52 +0200

    ai

---
+++ Assets/scripts/control/AiControler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AiControler : MonoBehaviour
+    {
+        [SerializeField] float chaseDist = 5f;
+
+        public void Update(){
+            GameObject player =  GameObject.FindWithTag("Player");
+        }
+    }
+
+}
